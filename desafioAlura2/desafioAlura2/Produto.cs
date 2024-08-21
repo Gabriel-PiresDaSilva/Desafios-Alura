4090546b58d1c0f24dbfb7228140cde618df2b90
Gabriel P Silva <132621830+Gabriel-PiresDaSilva@users.noreply.github.com>
2024-08-21 17:58:22 -0300

    Add files via upload

---
+++ desafioAlura2/desafioAlura2/Produto.cs
@@ -0,0 +1,29 @@
+﻿class Produto
+{
+    public Estoque Estoque { get; set; }
+
+    public void ExibirEstoque()
+    {
+        Console.WriteLine($"O produto {Estoque.Nome} contem {Estoque.Quantidade} unidades");
+    }
+}
+
+class Estoque
+{
+    public string Nome { get; set; }
+    public int Quantidade { get; set; }
+
+    public Estoque(string nome, int quantidade)
+    {
+        Nome = nome;
+        Quantidade = quantidade;
+    }
+
+    private List<Produto> produtos = new List<Produto>();
+
+    public void AdicionarProduto(Produto produto)
+    {
+        produtos.Add(produto);
+        produto.Estoque = this;
+    }
+}
---
+++ desafioAlura2/desafioAlura2/Program.cs
@@ -0,0 +1,6 @@
+﻿Estoque estoque = new Estoque("Açúcar", 100);
+Produto produto1 = new Produto();
+
+estoque.AdicionarProduto(produto1);
+produto1.ExibirEstoque();
+
