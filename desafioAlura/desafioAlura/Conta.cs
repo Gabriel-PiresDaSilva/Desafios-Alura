f508a4ec47ec11fcba49e87e064e7129b34246ee
Gabriel P Silva <132621830+Gabriel-PiresDaSilva@users.noreply.github.com>
2024-08-21 17:57:41 -0300

    Add files via upload

---
+++ desafioAlura/desafioAlura/Conta.cs
@@ -0,0 +1,24 @@
+﻿
+class Conta
+{
+    public Titular Nome { get; set; }
+    public int Agencia { get; set; }
+    public int NumConta { get; set; }
+    public int Saldo { get; set; }
+    public int Limite { get; set; }
+
+    public void ExibirInfoDetalhado()
+    {      
+        Console.WriteLine($"Nome do titular: {Nome.Nome}\n\nAgencia: {Agencia}\t Numero da conta: {NumConta}\t Saldo: {Saldo}\t Limite: {Limite}");
+    }
+}
+
+class Titular
+{
+    public string Nome { get; set; }
+
+    public Titular(string nome)
+    {
+        Nome = nome;
+    }
+}
---
+++ desafioAlura/desafioAlura/Program.cs
@@ -0,0 +1,10 @@
+﻿Conta conta1 = new Conta()
+{
+    Nome = new Titular("Gabriel"),
+    Agencia = 1,
+    NumConta = 2244566,
+    Saldo = 200,
+    Limite = 1000
+};
+
+conta1.ExibirInfoDetalhado(); 
